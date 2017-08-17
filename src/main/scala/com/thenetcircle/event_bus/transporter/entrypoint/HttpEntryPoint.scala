ceae5a83aa23e4f83aa8ad61517423334f1994f0
beineng <baineng.ma@gmail.com>
2017-08-17 17:54:15 +0800

    improve http entrypoint

--- src/main/scala/com/thenetcircle/event_bus/transporter/entrypoint/HttpEntryPoint.scala
+++ src/main/scala/com/thenetcircle/event_bus/transporter/entrypoint/HttpEntryPoint.scala
@@ -20,18 +20,17 @@ package com.thenetcircle.event_bus.transporter.entrypoint
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.Http.IncomingConnection
 import akka.http.scaladsl.model.{ HttpEntity, HttpRequest, HttpResponse, StatusCodes }
 import akka.stream._
-import akka.stream.scaladsl.{ Flow, Source }
-import akka.stream.stage.{ GraphStage, GraphStageLogic, InHandler, OutHandler }
-import com.thenetcircle.event_bus.EventFormat.DefaultFormat
+import akka.stream.scaladsl.{ Flow, GraphDSL, Source }
+import akka.stream.stage._
+import akka.util.ByteString
 import com.thenetcircle.event_bus._
 import com.thenetcircle.event_bus.extractor.Extractor
 
-import scala.collection.mutable
 import scala.concurrent.duration._
 import scala.concurrent.{ Future, Promise }
+import scala.util.control.NonFatal
 import scala.util.{ Failure, Success }
 
 case class HttpEntryPointSettings(
@@ -42,20 +41,42 @@ case class HttpEntryPointSettings(
 class HttpEntryPoint(settings: HttpEntryPointSettings)(implicit system: ActorSystem, materializer: Materializer)
     extends EntryPoint(settings) {
 
-  implicit val ec = system.dispatcher
+  implicit val _ec = system.dispatcher
 
-  val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
+  private val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
     Http().bind(interface = settings.interface, port = settings.port)
 
   override val port: Source[Source[Event, NotUsed], Future[Http.ServerBinding]] =
-    serverSource.map(connection => Source.fromGraph(new HttpEntryPointGraph(connection)))
+    serverSource
+      .map(connection => {
+
+        Source.fromGraph(GraphDSL.create() { implicit builder =>
+          import GraphDSL.Implicits._
+
+          val handlerFlowShape = builder.add(connection.flow)
+          val connectionHandler = builder.add(new HttpEntryPoint.ConnectionHandler())
+
+          handlerFlowShape.out ~> connectionHandler.in
+
+          connectionHandler.out0 ~> Flow[Future[HttpResponse]]
+            .mapAsync(1)(identity) ~> handlerFlowShape
+
+          SourceShape(connectionHandler.out1)
+        })
+
+      })
 
 }
 
 object HttpEntryPoint {
 
-  private class ConnectionHandler()(implicit extractor: Extractor[EventFormat])
+  final class ConnectionHandler()(implicit system: ActorSystem,
+                                  materializer: Materializer,
+                                  extractor: Extractor[EventFormat])
       extends GraphStage[FanOutShape2[HttpRequest, Future[HttpResponse], Event]] {
+
+    implicit val _ec = system.dispatcher
+
     val in: Inlet[HttpRequest] = Inlet("inlet-http-request")
     val out0: Outlet[Future[HttpResponse]] = Outlet("outlet-http-response")
     val out1: Outlet[Event] = Outlet("outlet-event")
@@ -68,59 +89,57 @@ object HttpEntryPoint {
         setHandler(
           in,
           new InHandler {
-            override def onPush(): Unit = {
-              val request = grab(in)
-              val (response, event) = requestPreprocess(request)
-
-              push(out0, response)
-
-              if (event.isDefined)
-                push(out1, event)
-            }
+            override def onPush(): Unit =
+              push(out0, requestPreprocess(grab(in)))
           }
         )
 
         setHandler(out0, new OutHandler {
-          override def onPull() = ???
+          override def onPull(): Unit = {}
         })
 
         setHandler(out1, new OutHandler {
-          override def onPull() = ???
+          override def onPull(): Unit =
+            pull(in)
         })
 
-        def requestPreprocess(request: HttpRequest): (Future[HttpResponse], Option[Event]) = {
+        def requestPreprocess(request: HttpRequest): Future[HttpResponse] = {
           val responsePromise = Promise[HttpResponse]
-          var event: Option[Event] = None
-          val data = request.entity.toStrict(3.seconds)
+          val data = request.entity.toStrict(3.seconds)(materializer)
+          val eventExtractCallback = getEventExtractCallback(responsePromise)
 
           data.onComplete {
             case Success(entity) =>
-              val body = entity.data
-              try {
-                val extractedData = extractor.extract(body)
-                event = Some(
-                  Event(
-                    extractedData.metadata,
-                    EventBody(body, extractor.dataFormat),
-                    extractedData.channel.getOrElse(""),
-                    EventSourceType.Http,
-                    extractedData.priority.getOrElse(EventPriority.Normal),
-                    Map.empty
-                  ).withCommitter(() => {
-                    responsePromise.success(HttpResponse())
-                  })
-                )
-              } catch {
-                case e: Throwable =>
-                  responsePromise.success(HttpResponse(StatusCodes.BadRequest, entity = HttpEntity(e.getMessage)))
-              }
-
+              eventExtractCallback.invoke(entity.data)
             case Failure(e) =>
               responsePromise.success(HttpResponse(StatusCodes.BadRequest, entity = HttpEntity(e.getMessage)))
           }
 
-          (responsePromise.future, event)
+          responsePromise.future
         }
+
+        def getEventExtractCallback(responsePromise: Promise[HttpResponse]): AsyncCallback[ByteString] =
+          getAsyncCallback(data => {
+            try {
+              val extractedData = extractor.extract(data)
+              push(
+                out1,
+                Event(
+                  extractedData.metadata,
+                  EventBody(data, extractor.dataFormat),
+                  extractedData.channel.getOrElse(""),
+                  EventSourceType.Http,
+                  extractedData.priority.getOrElse(EventPriority.Normal),
+                  Map.empty
+                ).withCommitter(() => {
+                  responsePromise.success(HttpResponse(entity = HttpEntity("event got consumer")))
+                })
+              )
+            } catch {
+              case NonFatal(e) =>
+                responsePromise.success(HttpResponse(StatusCodes.BadRequest, entity = HttpEntity(e.getMessage)))
+            }
+          })
 
       }
   }
