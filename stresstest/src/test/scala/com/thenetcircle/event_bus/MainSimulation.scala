c5f125b1871eeea4c988a6a9a8eb2e0f4d992f97
beineng <baineng.ma@gmail.com>
2017-09-26 18:12:44 +0800

    update kamon configuration

--- stresstest/src/test/scala/com/thenetcircle/event_bus/MainSimulation.scala
+++ stresstest/src/test/scala/com/thenetcircle/event_bus/MainSimulation.scala
@@ -70,7 +70,7 @@ class MainSimulation extends Simulation {
 
   setUp(
     scn.inject(
-      rampUsers(10) over (1 seconds)
+      rampUsers(50000) over (1 minute)
     )).protocols(httpConf)
 
 }
