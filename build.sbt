bb82351b97362451e17a729740bf526edc54a289
beineng <baineng.ma@gmail.com>
2018-01-02 16:56:36 +0800

    remote integration test depenencies

--- build.sbt
+++ build.sbt
@@ -1,8 +1,5 @@
 import Dependencies._
 
-def itFilter(name: String): Boolean = name endsWith "ISpec"
-def unitFilter(name: String): Boolean = (name endsWith "Spec") && !itFilter(name)
-
 lazy val eventBus = (project in file("."))
   .enablePlugins(JavaAppPackaging, SbtAspectj, BuildInfoPlugin)
   .settings(
@@ -12,12 +9,6 @@ lazy val eventBus = (project in file("."))
     libraryDependencies ++= mainDependencies,
     bashScriptExtraDefines += s"""addJava "${(aspectjWeaverOptions in Aspectj).value
       .mkString(" ")}""""
-  )
-  .configs(IntegrationTest)
-  .settings(
-    Defaults.itSettings,
-    testOptions in Test := Seq(Tests.Filter(unitFilter)),
-    testOptions in IntegrationTest := Seq(Tests.Filter(itFilter))
   )
 
 lazy val stressTest = (project in file("stresstest"))
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,37 +1,37 @@
 import sbt._
 
 object Dependencies {
-  private val akkaVersion     = "2.5.4"
+  private val akkaVersion = "2.5.4"
   private val akkaHttpVersion = "10.0.9"
 
   // Dependencies
-  private val akka              = "com.typesafe.akka" %% "akka-slf4j"          % akkaVersion
-  private val akkaStream        = "com.typesafe.akka" %% "akka-stream"         % akkaVersion
-  private val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion % "it,test"
-  private val akkaHttp          = "com.typesafe.akka" %% "akka-http"           % akkaHttpVersion
-  private val akkaHttpTestkit   = "com.typesafe.akka" %% "akka-http-testkit"   % akkaHttpVersion % "it,test"
-  private val akkaStreamKafka   = "com.typesafe.akka" %% "akka-stream-kafka"   % "0.17"
-  private val sprayJson         = "io.spray"          %% "spray-json"          % "1.3.3"
+  private val akka = "com.typesafe.akka" %% "akka-slf4j" % akkaVersion
+  private val akkaStream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
+  private val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion % Test
+  private val akkaHttp = "com.typesafe.akka" %% "akka-http" % akkaHttpVersion
+  private val akkaHttpTestkit = "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test
+  private val akkaStreamKafka = "com.typesafe.akka" %% "akka-stream-kafka" % "0.17"
+  private val sprayJson = "io.spray" %% "spray-json" % "1.3.3"
 
-  private val scalaTest      = "org.scalatest"              %% "scalatest"         % "3.0.1" % "it,test"
-  private val scalaLogging   = "com.typesafe.scala-logging" %% "scala-logging"     % "3.7.2"
-  private val logbackCore    = "ch.qos.logback"             % "logback-core"       % "1.2.2"
-  private val logbackClassic = "ch.qos.logback"             % "logback-classic"    % "1.2.3"
-  private val scalaUUID      = "io.jvm.uuid"                %% "scala-uuid"        % "0.2.3"
-  private val ficus          = "com.iheart"                 %% "ficus"             % "1.4.2"
-  private val akkaTracing    = "com.github.levkhomich"      %% "akka-tracing-core" % "0.6"
+  private val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1" % Test
+  private val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2"
+  private val logbackCore = "ch.qos.logback" % "logback-core" % "1.2.2"
+  private val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
+  private val scalaUUID = "io.jvm.uuid" %% "scala-uuid" % "0.2.3"
+  private val ficus = "com.iheart" %% "ficus" % "1.4.2"
+  private val akkaTracing = "com.github.levkhomich" %% "akka-tracing-core" % "0.6"
   // private val rediscala = "com.github.etaty"   %% "rediscala"                % "1.8.0"
   // private val akkaStreamAMQP  = "com.lightbend.akka" %% "akka-stream-alpakka-amqp" % "0.10"
 
   private val gatlingChartsHighcharts = "io.gatling.highcharts" % "gatling-charts-highcharts" % "2.2.2" % Test
-  private val gatlingTestFramework    = "io.gatling"            % "gatling-test-framework"    % "2.2.2" % Test
+  private val gatlingTestFramework = "io.gatling" % "gatling-test-framework" % "2.2.2" % Test
 
   private val kamonVersion = "0.6.7"
 
-  private val kamonCore          = "io.kamon" %% "kamon-core"           % kamonVersion
-  private val kamonAkka          = "io.kamon" %% "kamon-akka-2.5"       % kamonVersion
-  private val kamonStatsd        = "io.kamon" %% "kamon-statsd"         % kamonVersion
-  private val kamonLogReporter   = "io.kamon" %% "kamon-log-reporter"   % kamonVersion
+  private val kamonCore = "io.kamon" %% "kamon-core" % kamonVersion
+  private val kamonAkka = "io.kamon" %% "kamon-akka-2.5" % kamonVersion
+  private val kamonStatsd = "io.kamon" %% "kamon-statsd" % kamonVersion
+  private val kamonLogReporter = "io.kamon" %% "kamon-log-reporter" % kamonVersion
   private val kamonSystemMetrics = "io.kamon" %% "kamon-system-metrics" % kamonVersion
 
   val mainDependencies = Seq(
@@ -56,9 +56,5 @@ object Dependencies {
     kamonSystemMetrics
   )
 
-  val stressTestDependencies = Seq(
-    gatlingChartsHighcharts,
-    gatlingTestFramework,
-    akkaHttp
-  )
+  val stressTestDependencies = Seq(gatlingChartsHighcharts, gatlingTestFramework, akkaHttp)
 }
