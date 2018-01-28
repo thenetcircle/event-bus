be100416e4858e0658f907dd6177599d65ec8668
baineng <baineng.ma@gmail.com>
2018-01-28 10:58:17 +0800

    small updates

--- integration-test/src/test/scala/com/thenetcircle/event_bus/tasks/KafkaSourceTest.scala
+++ integration-test/src/test/scala/com/thenetcircle/event_bus/tasks/KafkaSourceTest.scala
@@ -2,16 +2,16 @@ package com.thenetcircle.event_bus.tasks
 
 import java.util.Properties
 
-import akka.stream.scaladsl.{Flow, Sink, Source}
-import akka.stream.testkit.{TestPublisher, TestSubscriber}
+import akka.stream.scaladsl.Flow
 import com.thenetcircle.event_bus.IntegrationTestBase
+import com.thenetcircle.event_bus.interfaces.EventStatus.Norm
 import com.thenetcircle.event_bus.interfaces.{Event, EventStatus}
 import com.thenetcircle.event_bus.tasks.kafka.extended.{EventSerializer, KafkaKey, KafkaKeySerializer}
-import com.thenetcircle.event_bus.tasks.kafka.{KafkaSource, KafkaSourceSettings, ProducerKey, ProducerValue}
+import com.thenetcircle.event_bus.tasks.kafka._
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.scalatest.BeforeAndAfter
 
-import scala.concurrent.duration._
+import scala.collection.mutable.ArrayBuffer
 
 class KafkaSourceTest extends IntegrationTestBase with BeforeAndAfter {
 
@@ -42,41 +42,62 @@ class KafkaSourceTest extends IntegrationTestBase with BeforeAndAfter {
   }
 
   it should "responds error when get non json request" in {
-    val testEvent = createTestEvent(
-      body = s"""
-        |{
-        |  "id": "eventid",
-        |  "title": "message.send",
-        |  "verb": "send",
-        |  "actor": {"id": "aid", "objectType": "atype"}
-        |}
-      """.stripMargin
-    )
 
-    val testSink   = TestSubscriber.probe[(EventStatus, Event)]
+    /*val testSink   = TestSubscriber.probe[(EventStatus, Event)]
     val testSource = TestPublisher.probe[(EventStatus, Event)](0)
     val testFlow =
-      Flow.fromSinkAndSource(Sink.fromSubscriber(testSink), Source.fromPublisher(testSource))
+      Flow.fromSinkAndSource(Sink.fromSubscriber(testSink), Source.fromPublisher(testSource))*/
 
+    var receivedEvents = ArrayBuffer.empty[Event]
+
+    val testFlow = Flow[(EventStatus, Event)].map {
+      case (_, event) =>
+        receivedEvents.synchronized(receivedEvents += event)
+        (Norm, event)
+    }
     kafkaSource.runWith(testFlow)
 
     // Three partitions will subscribe three times
-    val s1 = testSink.expectSubscription()
+    /*val s1 = testSink.expectSubscription()
     val s2 = testSink.expectSubscription()
     val s3 = testSink.expectSubscription()
     val s4 = testSource.expectSubscription()
-    s1.request(10); s2.request(10); s3.request(10)
+    s1.request(10); s2.request(10); s3.request(10)*/
 
-    producer.send(new ProducerRecord[ProducerKey, ProducerValue](testTopic, KafkaKey(testEvent), testEvent))
+    for (i <- 1 to 10) {
+      val testEvent = createTestEvent(
+        body = s"""
+                |{
+                |  "id": "event-id-$i",
+                |  "title": "message.send",
+                |  "verb": "send",
+                |  "actor": {"id": "aid$i", "objectType": "atype"}
+                |}
+      """.stripMargin
+      )
+      producer.send(new ProducerRecord[ProducerKey, ProducerValue](testTopic, KafkaKey(testEvent), testEvent))
+    }
     producer.flush()
 
-    Thread.sleep(100)
+    Thread.sleep(1000)
 
+    println(receivedEvents)
+
+    kafkaSource.shutdown()
+
+    Thread.sleep(1000)
+    println("--------------------------------")
+
+    kafkaSource.runWith(testFlow)
+
+    println(receivedEvents)
+
+    /*
     val (status, event) = testSink.expectNext(30.seconds)
 
     event.uuid shouldEqual "eventid"
 
-    s4.sendNext(status -> event)
+    s4.sendNext(status -> event)*/
   }
 
 }
